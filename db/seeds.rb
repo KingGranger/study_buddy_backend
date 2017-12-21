389722e39cbe3bc0d0d6a5fd8b86039ad521440d
github username <github email address>
2017-12-21 17:54:45 -0500

    yeah brandon totally loves short shorts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+def subjects
+  subs = ['business', 'computer science', 'english', 'life skills', 'foreign language', 'math', 'performing arts', 'physical education', 'science', 'social studies', 'art', 'social science']
+
+  subs.each do |sub|
+    Subject.create(subject_name: sub)
+  end
+end
+
+def templates
+  templates = ['really good', 'cool', 'could be better', 'nice', 'sloppy mess', '100-fire-100', 'do not use this one']
+
+  templates.each do |temp|
+    Template.create(name: temp)
+  end
+end
+
+def types
+  types = ['bullet', 'definition', 'img', 'text', 'code', 'equation', 'figure']
+
+  types.each do |type|
+    Type.create(name: type)
+  end
+end
